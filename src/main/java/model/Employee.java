3f0f47f789572d6a2e674e9b12615e0096917203
Navaneeth <162485789+Navaneeth-V@users.noreply.github.com>
2025-07-20 13:01:52 +0530

    created models

---
+++ src/main/java/model/Employee.java
@@ -0,0 +1,53 @@
+package model;
+
+public class Employee {
+    private int id;
+
+    private String name;
+
+    private String designation;
+
+    private int departmentId;
+
+    private double salary;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
