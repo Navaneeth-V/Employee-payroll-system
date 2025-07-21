200e3a8c98922dd38e5edf1515814e595581ad58
Navaneeth <162485789+Navaneeth-V@users.noreply.github.com>
2025-07-21 20:35:08 +0530

    blueprint for employee service

--- src/main/java/service/EmployeeService.java
+++ src/main/java/service/EmployeeService.java
@@ -1,4 +1,32 @@
 package service;
 
+import model.Employee;
+
+import java.util.ArrayList;
+
 public class EmployeeService {
+
+    public void addEmployee() {
+
+    }
+
+    public Employee getEmployeeById(int id) {
+        return null;
+    }
+
+    public ArrayList<Employee> getAllEmployees() {
+        return null;
+    }
+
+    public void updateEmployee() {
+
+    }
+
+    public void deleteEmployee(int id) {
+
+    }
+
+    public void ListEmployeesByDepartment(int departmentId) {
+
+    }
 }
